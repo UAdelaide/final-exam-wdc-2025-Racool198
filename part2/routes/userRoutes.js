03245cd47b9b920417222e42ee47fbc9149e664b
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 14:57:35 +0000

    [automatic commit] 2025-06-20 14:57:35 UTC 121.45.87.215

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -58,6 +58,7 @@ router.post('/login', async (req, res) => {
     req.session.role = rows[0].role;
     req.session.user_id = rows[0].user_id;
 
+    console.log("redirecting");
     res.redirect("/owner-dashboard.html");
 
     // res.json({ message: 'Login successful', user: rows[0] });
