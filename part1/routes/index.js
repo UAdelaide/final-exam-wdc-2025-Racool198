30c31e4f4a662d2be3be9e8ea7c54e05dec9f2d2
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 10:10:56 +0000

    [automatic commit] 2025-06-20 10:10:56 UTC 121.45.87.215

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -5,14 +5,14 @@ var mysql = require('mysql2/promise');
 
 let db;
 
-(async () => {
-  db = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'DogWalkService'
-  });
-})();
+// (async () => {
+//   db = await mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'DogWalkService'
+//   });
+// })();
 
 
 /* GET home page. */
@@ -21,10 +21,9 @@ router.get('/api/dogs', async function(req, res, next) {
     const rows = await db.execute("SELECT name AS dog_name,size,(SELECT username FROM Users WHERE Users.user_id = Dogs.owner_id) AS owner_username FROM Dogs;");
     res.json(rows[0]);
   } catch (err){
-    res.status(404)
-    res.send("Error Occured")
+    res.status(404);
+    res.send("Error Occured");
   }
-
 });
 
 router.get('/api/walkrequests/open', async function(req, res, next) {
