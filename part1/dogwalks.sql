39ec15d32c4c28c2097d40cd6c618e12675306f1
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 08:04:06 +0000

    [automatic commit] 2025-06-20 08:04:06 UTC 121.45.87.215

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -65,10 +65,7 @@ VALUES
 
 
 INSERT INTO Dogs (owner_id, name, size)
-
-
-    dog_id INT AUTO_INCREMENT PRIMARY KEY,
-    owner_id INT NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    size ENUM('small', 'medium', 'large') NOT NULL,
-    FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+VALUES
+(
+    (SELECT)
+)
