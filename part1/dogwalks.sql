f85bfaf39dce4f246a2835445f24ae9d5936b48a
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 08:05:06 +0000

    [automatic commit] 2025-06-20 08:05:06 UTC 121.45.87.215

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -66,7 +66,4 @@ VALUES
 
 INSERT INTO Dogs (owner_id, name, size)
 VALUES
-(
-    (SELECT user_id FROM Users WHERE username = "alice123"),
-    "MAX"
-)
+((SELECT user_id FROM Users WHERE username = "alice123"),"MAX", "medium")
