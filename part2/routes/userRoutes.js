46af473b5be0770c75414318df2652450c3af76c
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 12:34:39 +0000

    [automatic commit] 2025-06-20 12:34:39 UTC 121.45.87.215

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,7 +37,7 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  console.log(req);
+  console.log(req.body);
   const username = req.body.username;
   const password = req.body.password;
   console.log(username);
