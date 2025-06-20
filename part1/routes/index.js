b279011dcb0df8e362c55fea899f89245c363e47
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 08:52:04 +0000

    [automatic commit] 2025-06-20 08:52:04 UTC 121.45.87.215

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -21,7 +21,7 @@ router.get('/api/dogs', async function(req, res, next) {
   res.json(rows[0]);
 });
 
-router.get('/api/walkrequests', async function(req, res, next) {
+router.get('/api/walkrequests/open', async function(req, res, next) {
   const rows = await db.execute("SELECT name AS dog_name,size,(SELECT username FROM Users WHERE Users.user_id = Dogs.owner_id) AS owner_username FROM Dogs;");
   res.json(rows[0]);
 });
