d5ed1fc3198fcfc7c9f82a014054e804929075e4
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 09:05:10 +0000

    [automatic commit] 2025-06-20 09:05:10 UTC 121.45.87.215

--- part1/app.js
+++ part1/app.js
@@ -68,7 +68,7 @@ let db;
     `);
 
     await db.execute(`
-      INSERT INTO WalkRatings (request_id, requested_time, duration_minutes, location, status)
+      INSERT INTO WalkRatings (rating, comments)
       VALUES
       ((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-10 08:00:00", "30", "Parklands", "open"),
       ((SELECT dog_id FROM Dogs WHERE name = "Bella"), "2025-06-10 09:30:00", "45", "Beachside Ave", "accepted"),
