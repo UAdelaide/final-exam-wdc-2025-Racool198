d775e6c8c4a04afe27229831a12ad1f537f3d31e
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 17:25:36 +0000

    [automatic commit] 2025-06-20 17:25:36 UTC 121.45.87.215

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -14,14 +14,15 @@ router.get('/', async (req, res) => {
     `);
     res.json(rows);
   } catch (error) {
-    console.error('SQL Error:', error);
+    // console.error('SQL Error:', error);
     res.status(500).json({ error: 'Failed to fetch walk requests' });
   }
 });
 
 // POST a new walk request (from owner)
 router.post('/', async (req, res) => {
-  const { dog_id, requested_time, duration_minutes, location } = req.body;
+  const {
+     dog_id, requested_time, duration_minutes, location } = req.body;
 
   try {
     const [result] = await db.query(`
