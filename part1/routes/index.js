adbb6d999fafc9e52b1e259a5e15f008c84e419a
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 08:31:16 +0000

    [automatic commit] 2025-06-20 08:31:16 UTC 121.45.87.215

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql2/promise');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/api/dogs', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
