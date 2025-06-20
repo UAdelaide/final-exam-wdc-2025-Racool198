ced449195e8ab44a195cad770f4aaf73ec3a442a
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 09:17:54 +0000

    [automatic commit] 2025-06-20 09:17:54 UTC 121.45.87.215

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -37,7 +37,7 @@ router.get('/api/walkrequests/open', async function(req, res, next) {
 router.get('/api/walkers/summary', async function(req, res, next) {
   const rows = await db.execute(`SELECT username
     FROM Users;
-    WHERE Users.role = "walker";`);
+    WHERE role = "walker";`);
   res.json(rows[0]);
 });
 
