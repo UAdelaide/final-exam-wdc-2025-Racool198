e8a6229828e2e7b1fbe0f5243e4c619b3db17a0f
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 09:51:48 +0000

    [automatic commit] 2025-06-20 09:51:48 UTC 121.45.87.215

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -81,4 +81,4 @@ CREATE TABLE WalkRatings (
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES
-((SELECT))
+((SELECT dog_id FROM Dogs WHERE name = "Max"), "")
