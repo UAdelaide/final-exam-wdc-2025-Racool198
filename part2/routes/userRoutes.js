0031c39fb3277aa0462710ad01eb47fa72410c65
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 14:36:01 +0000

    [automatic commit] 2025-06-20 14:36:01 UTC 121.45.87.215

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -53,7 +53,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    req.session.
+    req.session.role = rows[0][""]
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
