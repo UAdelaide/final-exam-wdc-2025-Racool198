c43ed68c2249c2b0cac4d4786f8a29fc0affd08a
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 09:55:30 +0000

    [automatic commit] 2025-06-20 09:55:30 UTC 121.45.87.215

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -82,5 +82,6 @@ VALUES
 INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
 VALUES
 (
-    (SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = "Max"))
+    (SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = "Max")),
+    (SELECT user_id FROM Users WHERE username = )
 );
