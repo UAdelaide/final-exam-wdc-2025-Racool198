9013ccca5a023132d9a80d44fa2e5e192bd9a9ec
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 14:50:12 +0000

    [automatic commit] 2025-06-20 14:50:12 UTC 121.45.87.215

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -29,6 +29,7 @@ router.post('/register', async (req, res) => {
 });
 
 router.get('/me', (req, res) => {
+  console.log(req.session.user_id);
   if (!req.session.user_id) {
     return res.status(401).json({ error: 'Not logged in' });
   }
