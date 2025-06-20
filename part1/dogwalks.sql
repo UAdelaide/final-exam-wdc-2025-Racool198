12905b72fb080a3b2c2d956d95f0870460272255
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 07:56:22 +0000

    [automatic commit] 2025-06-20 07:56:22 UTC 121.45.87.215

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -59,5 +59,5 @@ VALUES
 ("alice123", "alice@example.com", "hashed123", "owner")
 ("bobwalker", "bob@example.com", "hashed456", "walker")
 ("carol123", "carol@example.com", "hashed789", "owner")
-("alice123", "alice@example.com", "hashed123", "owner")
-("alice123", "alice@example.com", "hashed123", "owner")
+("adamowner", "adam@example.com", "hashed123", "owner")
+("billwalker", "alice@example.com", "hashed123", "owner")
