c98d74e5a82fa1e71602f749043059e77f25f5d9
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 08:14:10 +0000

    [automatic commit] 2025-06-20 08:14:10 UTC 121.45.87.215

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -76,6 +76,6 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 VALUES
 ((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-10 08:00:00", "30", "Parklands", "open"),
 ((SELECT dog_id FROM Dogs WHERE name = "Bella"), "2025-06-10 09:30:00", "45", "Beachside Ave", "accepted"),
-((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-9 08:00:00", "30", "Adelaide", "open"),
-((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-8 08:00:00", "30", "Klemzig", "open"),
-((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-11 08:00:00", "30", "Greenpath", "open");
+((SELECT dog_id FROM Dogs WHERE name = "Jake"), "2025-06-9 08:00:00", "30", "Adelaide", "open"),
+((SELECT dog_id FROM Dogs WHERE name = "Finn"), "2025-06-8 08:00:00", "30", "Klemzig", "open"),
+((SELECT dog_id FROM Dogs WHERE name = "Cat"), "2025-06-11 08:00:00", "30", "Greenpath", "open");
