587edd1447a3963f84150d17b8e455670e0fa05b
Rahul Satheesan <rahulpanayal18@gmail.com>
2025-06-20 17:24:55 +0000

    [automatic commit] 2025-06-20 17:24:55 UTC 121.45.87.215

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -83,7 +83,8 @@ router.post('/logout', (req, res) => {
 
 
 // POST login (dummy version)
-// Minor modification made to the login router to change email field to username and store role and user_id in the session and send the appropriate redirect based on role.
+// Minor modification made to the login router to change email field to username
+// changed store role and user_id in the session and send the appropriate redirect based on role.
 router.post('/login', async (req, res) => {
   // const data = req.body;
   const username = req.body.username;
